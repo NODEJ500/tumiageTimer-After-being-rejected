c4b9e632130acf13bba5d4e156895065741e223f
onodera <cxiaoyesi745@gmail.com>
2021-10-09 22:34:18 +0900

    Initial Commit

---
+++ originalAppTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  originalAppTest1
+//
+//  Created by Jun on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
