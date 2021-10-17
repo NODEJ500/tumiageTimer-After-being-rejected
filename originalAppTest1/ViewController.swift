8cd3d335dcb8d246e55de91a1f5852b10269292b
onodera <cxiaoyesi745@gmail.com>
2021-10-17 21:36:31 +0900

    3

--- originalAppTest1/ViewController.swift
+++ originalAppTest1/ViewController.swift
@@ -10,23 +10,32 @@ class ViewController: UIViewController {
     var startTime: TimeInterval? = nil
     var timer = Timer()
     var timermove = false
+    var suspend = false
     
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButtonVar: UIButton!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        let buttonTitle = startButtonVar.currentTitle
+       
     }
     
-    @IBAction func startButtonAction(_ sender: Any) {
+    @IBAction func startButtonAction(_ sender: UIButton!) {
         
-        if timermove == false {
-            timer.invalidate()
+        // let buttonTitle = self.startButtonVar.currentTitle
+        
+        if  timermove == false {
             self.startTime = Date.timeIntervalSinceReferenceDate
             self.timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
             timermove = true
-            showAnimation()
-        
+            sender.setTitle("ストップ", for: .normal)
         }
+        
+        self.timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+
     }
     @objc func timerCounter() {
         guard let startTime = self.startTime else { return }
@@ -37,21 +46,11 @@ class ViewController: UIViewController {
         self.timerLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
     }
     
-    @IBAction func stopButtonAction(_ sender: Any) {
-        
-        if timermove == true {
-            timer.invalidate()
-            timermove = false
-            
-            
-        }
-    
-    }
-    
     @IBAction func resetButtonAction(_ sender: Any){
         
         if timermove == false {
             self.startTime = nil
+            timer.invalidate()
             self.timerLabel.text = "00:00:00"
        }
     }
@@ -59,12 +58,11 @@ class ViewController: UIViewController {
     func showAnimation() {
         
         let animationView = AnimationView(name: "Animation")
-        animationView.layer.position = CGPoint(x: 0, y: 0)
         animationView.center = self.view.center
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
         animationView.loopMode = .loop
         animationView.animationSpeed = 2.5
         view.addSubview(animationView)
         animationView.play()
-    
     }
 }
