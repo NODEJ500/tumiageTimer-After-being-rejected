c3fcbce12ebdf64efa39e1e5c31cc2c85a452e87
onodera <cxiaoyesi745@gmail.com>
2021-10-24 16:06:14 +0900

    twitter 1

--- originalAppTest1/ViewController.swift
+++ originalAppTest1/ViewController.swift
@@ -9,11 +9,16 @@ class ViewController: UIViewController {
     
     var timer:Timer!
     var time:Int = 0
+    var tweetComent = "今日の積み上げ!"
+    
     
     @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+       
+        
     }
     
     @IBAction func startButtonAction(_ sender: UIButton!) {
@@ -33,12 +38,12 @@ class ViewController: UIViewController {
         let hour = Int(time / 3600)
         let min = Int(time - (hour * 3600)) / 60
         let sec = Int(time) % 60
-        self.timerLabel.text = String(format: "%02d:%02d", hour, min, sec)
+        self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, min, sec)
     }
     
-    @IBAction func resetButtonAction(_ sender: Any){
+    @IBAction func resetButtonAction(_ sender: Any) {
         
-        if timer != nil{
+        if timer != nil {
             timer.invalidate()
             timer = nil
         }
@@ -55,4 +60,16 @@ class ViewController: UIViewController {
         view.addSubview(animationView)
         animationView.play()
     }
-}
+    
+    
+    @IBAction func twitterShareButton(_ sender: Any) {
+        
+            //つぶやき画面を開く
+            let url = NSURL(string: "https://twitter.com/intent/tweet?text=")
+            if UIApplication.shared.canOpenURL(url! as URL) {
+                
+                UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
+            }
+        }
+    }
+
